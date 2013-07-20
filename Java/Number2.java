d5474abe55852dd3346ee9e38bf1480448d080ff
Sameet Sapra <sameetandpotatoes@gmail.com>
2013-07-19 22:37:26 -0500

    Updated Number2 and Number4 Java

--- Java/Number2.java
+++ Java/Number2.java
@@ -2,22 +2,18 @@ public class Number2
 {
 	public static void main(String[] args)
 	{
-		int counter = 0;
-		System.out.println(calcFib (counter));
-	}
-	public static int calcFib (int counter)
-	{
-		int a = 0, b = 0, c = 1;
+		Stopwatch s = new Stopwatch();
+		int a = 0, b = 0, c = 1, sum = 0;
 		while (a < 1000000)
 		{
 			a = b + c;
-			if (a % 2 == 0) counter+=a;
+			if (a % 2 == 0) sum+=a;
 			b = c + a;
-			if (b % 2 == 0) counter+=b;
+			if (b % 2 == 0) sum+=b;
 			c = a + b;
-			if (c % 2 == 0) counter+=c;
+			if (c % 2 == 0) sum+=c;
 		}
-		return counter;
+		System.out.println(sum);
+		System.out.println("Time: " + s.elapsedTime());
 	}
-
 }
--- Java/Number4.java
+++ Java/Number4.java
@@ -2,21 +2,33 @@ public class Number4
 {
 	public static void main(String[] args)
 	{
-		int a = 100;
-		int b = 100;
-		for (a = 100; b <= 1000 && a  <= 999; b++)
+		Stopwatch s = new Stopwatch();
+		int max = 0;
+		for (int a = 999,b = 999; b >= 100 && a  >= 100; b--)
 		{
-			if (b == 1000)
+			if (b == 100)
 			{
-				a++;
-				b = 100;
+				a--;
+				b = 999;
 			}
 			int p = a * b;
-			String palindrome = Integer.toString(p);
-			if (palindrome.equals(new StringBuffer(String.valueOf(palindrome)).reverse().toString())) 
+			if(checkPalindrome(p))
 			{
-				System.out.println(palindrome + " is a palindrome number; " + a + " x " + b);
+				if (p > max)
+					max = p;
+			}
+			if (a <= 800 && b <= 800)
+			{
+				System.out.println(max);
+				System.out.println("Time: " + s.elapsedTime());
+				break;
 			}	
 		}
+
+	}
+	public static boolean checkPalindrome(int product)
+	{
+		String palindrome = Integer.toString(product);
+		return (palindrome.equals(new StringBuffer(String.valueOf(palindrome)).reverse().toString()));
 	}
 }
