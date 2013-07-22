ce11358a256d4f961a189d64a0c40a9081596fea
Sameet Sapra <sameetandpotatoes@gmail.com>
2013-07-22 16:59:25 -0500

    Fixed Number13

--- Java/Number13.java
+++ Java/Number13.java
@@ -6,20 +6,20 @@ public class Number13
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter input file: ");
-		String fileInputName = input.nextLine();
+		Stopwatch s = new Stopwatch();
+		String fileInputName = "Number13.txt";
 		FileReader reader = new FileReader(fileInputName);
 		Scanner in = new Scanner(reader);
 		BigInteger sum = new BigInteger("0");
 		while (in.hasNextLine())
 		{
 			BigInteger lineONE = new BigInteger(in.nextLine());
-			BigInteger lineTWO = new BigInteger(in.nextLine()); 
+			BigInteger lineTWO = new BigInteger(in.nextLine());
 			sum = sum.add(lineONE.add(lineTWO));
 		}
-		
-		System.out.println("Answer: " + sum.toString().substring(0, 10));
+
+		System.out.println(sum.toString().substring(0, 10));
+		System.out.println(s.elapsedTime());
 	}
 
 }
