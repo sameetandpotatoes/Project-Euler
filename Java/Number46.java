882fd520a2e3917369da7b5f82d8d59d277cc62a
Sameet Sapra <sameetandpotatoes@gmail.com>
2013-08-04 15:41:06 -0500

    Finished 42 and 46

--- Java/Number46.java
+++ Java/Number46.java
@@ -1,11 +1,55 @@
+import java.util.ArrayList;
 public class Number46
 {
 	public static void main(String[] args)
 	{
+		Stopwatch s = new Stopwatch();
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		for (int number = 2; number < 10000; number++)
+		{
+			if(checkPrime(number))
+				primes.add(number);
+		}
+		for (int number = 3; number <= 100000; number+=2)
+		{
+			if (!checkPrime(number))
+			{
+				boolean foundResult = false;
+				for (int prime : primes)
+				{
+					if (number - prime < 0)
+						break;
+					double subtract = (number - prime);
+					for (int i = 1; i < 500; i++)
+					{
+						if (subtract == 2 * (i * i) &&  (subtract == Math.rint(subtract)))
+						{
+							foundResult = true;
+							break;
+						}
+					}
+					if (foundResult)
+						break;
+				}
+				if (!foundResult)
+				{
+					System.out.println(number);
+					System.out.println(s.elapsedTime());
+					break;
+				}
+			}
+		}
 	}
-
-	private static int getComposite(int number)
+	public static boolean checkPrime(int number)
 	{
-		return 0;
+		int divisor = 2;
+		while (divisor <= (int)(Math.sqrt(number)))
+		{
+			if (number % divisor == 0)
+				return false;
+			divisor ++;
+		}
+		return true;
 	}
 }
+
