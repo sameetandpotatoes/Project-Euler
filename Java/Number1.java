e2d1970399941579a1322f7bfaf4369295d5699c
Sameet Sapra <sameetandpotatoes@gmail.com>
2013-08-04 12:44:38 -0500

    Updated files

--- Java/Number1.java
+++ Java/Number1.java
@@ -1,14 +1,14 @@
-public class Number1 
+public class Number1
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		Stopwatch s = new Stopwatch();
 		int sum = 0;
 		for(short number = 1; number < 1000; number++)
 		{
-			if (number % 3 == 0) 
+			if (number % 3 == 0)
 				sum += number;
-			else if (number % 5 == 0) 
+			else if (number % 5 == 0)
 				sum += number;
 		}
 		System.out.println(sum);
