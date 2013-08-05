60e8f02271a2c95ca5204dd7941337e0eef692fe
Sameet Sapra <sameetandpotatoes@gmail.com>
2013-08-05 10:34:52 -0500

    Recoded 56, 63, 74

--- Java/Number56.java
+++ Java/Number56.java
@@ -1,8 +1,9 @@
 import java.math.BigInteger;
 public class Number56
-{	
-	public static void main(String[] args) 
+{
+	public static void main(String[] args)
 	{
+		Stopwatch s = new Stopwatch();
 		BigInteger a = new BigInteger("1");
 		int b = 2;
 		BigInteger max = new BigInteger("0");
@@ -14,50 +15,23 @@ public class Number56
 				b++;
 			}
 			BigInteger product = a.pow(b);
-			digitAdder(product, max, a, b);
-			//System.out.println(a + "^" + b + ": " + product);
+			int sum = digitAdder(product);
+			if (sum > max.intValue())
+				max = BigInteger.valueOf(sum);
 			a = a.add(new BigInteger("1"));
-			//max.equals(new BigInteger("0"));
 		}
+		System.out.println(max.intValue());
+		System.out.println(s.elapsedTime());
 	}
-	public static void digitAdder(BigInteger product, BigInteger max, BigInteger a, int b)
+	public static int digitAdder(BigInteger product)
 	{
-		BigInteger one = new BigInteger("1");
-		BigInteger sum = new BigInteger("0");
-		while(product.compareTo(one) >= 1)
+		int sum = 0;
+		while(product.compareTo(new BigInteger("1")) >= 1)
 		{
-			BigInteger remainder = product.remainder(new BigInteger("10")); 
-			sum = sum.add(remainder);
+			BigInteger remainder = product.remainder(new BigInteger("10"));
+			sum += remainder.intValue();
 			product = product.divide(new BigInteger("10"));
 		}
-		if (sum.compareTo(max) > max.intValue())
-		{
-			max = sum;
-			System.out.println("The maximum so far is " + max + "(" + a + "^" + b + ")");
-		}
-		/*int sum = 0, i = 0, j = 0, max = 0;
-		String s= "";
-		while(i < 100)
-		{
-			while(j < 100)
-			{
-				int product=(int) Math.pow(i,j);
-				s= "" + product; //this will convert the number into string
-				sum = 0;
-				for(int k = 0; k < s.length(); k++)
-				{
-					int a = product%10;// a stores each digit from right to left
-					product/=10;  //now value of product is a number with  last digit of original value removed
-					sum+=a;	
-				}
-				if(sum > max)
-				{
-					max=sum;
-				}	
-				j++;
-			}
-			i++;
-		}
-		System.out.print("maximum digital sum is :"+max);*/
+		return sum;
 	}
 }
--- Java/Number74.java
+++ Java/Number74.java
@@ -1,184 +1,62 @@
+import java.util.ArrayList;
 public class Number74
 {
 	public static void main(String[] args)
 	{
-		int number = 3, origNum = 3, countLoops = 0, counter = 0, answerCounter = 0;
-		while (origNum < 1000000)
+		Stopwatch s = new Stopwatch();
+		int finalCount = 0;
+		for (int startingNumber = 3; startingNumber < 200; startingNumber++)
 		{
-			if (countLoops == 0) origNum = number;
-			switch(Integer.toString(number).length())
+			int countTerms = 0;
+			ArrayList<Integer> usedTerms = new ArrayList<Integer>();
+			boolean repeatTerms = false;
+			int copyNumber = startingNumber;
+			System.out.print("NUMBER " + copyNumber + " ");
+			while (countTerms < 60)
 			{
-				case 1:
+				int sumDigits = 0;
+				while (copyNumber >= 1)
 				{
-					number = one(number);
-					if (number == 69 || number == 871 || number == 872)
-					{
-						origNum++;
-						countLoops = 0;
-						counter = 0;
-						break; 
-					}
-					else
-					{
-						counter++;
-						if (counter == 60) answerCounter++;
-					}
+					int digit = copyNumber % 10;
+					sumDigits += factorial(digit);
+					copyNumber = copyNumber / 10;
 				}
-				case 2:
+				for (int used : usedTerms)
 				{
-					number = two(number);
-					if (number == 69 || number == 871 || number == 872)
+					if (used == sumDigits)
 					{
-						origNum++;
-						countLoops = 0;
-						counter = 0;
-						break; 
-					}
-					else
-					{
-						counter++;
-						if (counter == 60) answerCounter++;
-					}
-				}
-				case 3: 
-				{
-					number = three(number);
-					if (number == 69 || number == 871 || number == 872)
-					{
-						origNum++;
-						countLoops = 0;
-						counter = 0;
+						//In a loop
+						repeatTerms = true;
 						break;
 					}
-					else	
-					{
-						counter++;
-						if (counter == 60) answerCounter++;
-					}
 				}
-				case 4:
-				{
-					number = four(number);
-					if (number == 69 || number == 871 || number == 872)
-					{	
-						origNum++;
-						countLoops = 0;
-						counter = 0;
-						break;
-					}
-					else
-					{
-						counter++;
-						if (counter == 60) answerCounter++;
-					}
-				}
-				case 5:
-				{	
-					number = five(number); 
-					if (number == 69 || number == 871 || number == 872)
-					{
-						origNum++;
-						countLoops = 0;
-						counter = 0;
-						break;
-					}
-					else
-					{
-						counter++;
-						if (counter == 60) answerCounter++;
-					}
-				}
-				case 6:
-				{
-					number = six(number);
-					if (number == 69 || number == 871 || number == 872)
-					{	
-						origNum++;
-						countLoops = 0;
-						counter = 0;
-						break;
-					}
-					else
-					{
-						counter++;
-						if (counter == 60) answerCounter++;
-					}	
-				}
+				if (repeatTerms)
+					break;
+				usedTerms.add(sumDigits);
+				countTerms++;
+				copyNumber = sumDigits;
+				System.out.print(copyNumber + " ");
+			}
+			System.out.println("N: " + countTerms);
+			if (countTerms == 60)
+			{
+				finalCount++;
 			}
 		}
-		System.out.println("Answer: " + answerCounter);
+		System.out.println(finalCount);
+		System.out.println(s.elapsedTime());
 	}
-	private static int six(int number)
+	public static int factorial(int i)
 	{
-		int a = 1, b = 1, c = 1, d = 1, e = 1, f = 1;
-		int one = Integer.parseInt(Integer.toString(number).substring(0, 1));
-		for (a = 1; one != 0; one--) a*=one; 
-		int two = Integer.parseInt(Integer.toString(number).substring(1, 2));
-		for (b = 1; two != 0; two--) b*=two; 
-		int three = Integer.parseInt(Integer.toString(number).substring(2, 3));
-		for (c = 1; three != 0; three--) c*=three; 
-		int four = Integer.parseInt(Integer.toString(number).substring(3, 4));
-		for (d = 1; four != 0; four--) d*=four; 
-		int five = Integer.parseInt(Integer.toString(number).substring(4, 5));
-		for (e = 1; five != 0; five--) e*=five; 
-		int six = Integer.parseInt(Integer.toString(number).substring(5, 6));
-		for (f = 1; six != 0; six--) f*=six; 
-		return a + b + c + d + e + f;
+		if (i == 1)
+		{
+			return 1;
+		}
+		else if (i == 0)
+		{
+			return 0;
+		}
+		else
+			return i * factorial(i-1);
 	}
-	private static int five(int number)
-	{
-		int a = 1, b = 1, c = 1, d = 1, e = 1;
-		int one = Integer.parseInt(Integer.toString(number).substring(0, 1));
-		for (a = 1; one != 0; one--) a*=one; 
-		int two = Integer.parseInt(Integer.toString(number).substring(1, 2));
-		for (b = 1; two != 0; two--) b*=two; 
-		int three = Integer.parseInt(Integer.toString(number).substring(2, 3));
-		for (c = 1; three != 0; three--) c*=three; 
-		int four = Integer.parseInt(Integer.toString(number).substring(3, 4));
-		for (d = 1; four != 0; four--) d*=four; 
-		int five = Integer.parseInt(Integer.toString(number).substring(4, 5));
-		for (e = 1; five != 0; five--) e*=five; 
-		return a + b + c + d + e;
-	}
-	private static int four(int number)
-	{
-		int a = 1, b = 1, c = 1, d = 1;
-		int one = Integer.parseInt(Integer.toString(number).substring(0, 1));
-		for (a = 1; one != 0; one--) a*=one; 
-		int two = Integer.parseInt(Integer.toString(number).substring(1, 2));
-		for (b = 1; two != 0; two--) b*=two; 
-		int three = Integer.parseInt(Integer.toString(number).substring(2, 3));
-		for (c = 1; three != 0; three--) c*=three; 
-		int four = Integer.parseInt(Integer.toString(number).substring(3, 4));
-		for (d = 1; four != 0; four--) d*=four; 
-		return a + b + c + d;
-	}
-	private static int three(int number)
-	{
-		int a = 1, b = 1, c = 1;
-		int one = Integer.parseInt(Integer.toString(number).substring(0, 1));
-		for (a = 1; one != 0; one--) a*=one; 
-		int two = Integer.parseInt(Integer.toString(number).substring(1, 2));
-		for (b = 1; two != 0; two--) b*=two; 
-		int three = Integer.parseInt(Integer.toString(number).substring(2, 3));
-		for (c = 1; three != 0; three--) c*=three; 
-		return a + b + c;
-	}
-	private static int two(int number)
-	{
-		int a = 1, b = 1;
-		int one = Integer.parseInt(Integer.toString(number).substring(0, 1));
-		for (a = 1; one != 0; one--) a*=one; 
-		int two = Integer.parseInt(Integer.toString(number).substring(1, 2));
-		for (b = 1; two != 0; two--) b*=two; 
-		return a + b;
-	}
-	private static int one(int number)
-	{
-		int a = 1;
-		int one = Integer.parseInt(Integer.toString(number).substring(0, 1));
-		for (a = 1; one != 0; one--) a*=one; 
-		return a;
-	}
-
 }
